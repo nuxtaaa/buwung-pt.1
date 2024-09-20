79c8729f0b29ffcd6e5528c2703185619a430afb
Muhammad Ziya Shidqi Elkamil <asterixgalia8@gmail.com>
2024-09-21 00:13:19 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,27 @@
+import java.awt.Component;
+
+import javax.swing.*;
+
+public class App {
+    private static Component flappyBird;
+
+    public static void main(String[] args) throws Exception {
+        int boardWidth = 360;
+        int boardHeight = 640;
+
+
+        JFrame frame = new JFrame("FlappyBird");
+        // frame.setVisible(true);                                  
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack();
+        flappyBird.requestFocus();
+        frame.setVisible(true);
+    
+    }
+}
